3ccb5fd7b2f2231b9933a66c4bf01ff079436b74
ThauMish <MjA0MjEwMWFjMWY2ZT2YzNjcyZTZkN2M@proton.me>
2024-07-12 12:27:54 +0200

    [ADD] Custom nvim conf - nvchad + lsp + filetype + ollama

--- config/neovim.nix
+++ config/neovim.nix
@@ -22,10 +22,13 @@ in
         luajitPackages.lua-lsp
         nil
         rust-analyzer
-        #nodePackages.bash-language-server
         yaml-language-server
         pyright
         marksman
+        docker-compose
+        docker
+        typescript-language-server
+        bash-language-server
       ];
       plugins = with pkgs.vimPlugins; [
         alpha-nvim
@@ -52,16 +55,43 @@ in
         nvim-ts-context-commentstring
         plenary-nvim
         neodev-nvim
-        luasnip
         telescope-nvim
         todo-comments-nvim
         nvim-tree-lua
         telescope-fzf-native-nvim
         vim-tmux-navigator
+        gitsigns-nvim
+        nvim-lsp-installer
+        which-key-nvim
+        semgrep
       ];
       extraConfig = ''
         set noemoji
         nnoremap : <cmd>FineCmdline<CR>
+        augroup filetype_yaml_ansible
+          autocmd!
+          autocmd BufRead,BufNewFile */roles/*.yml,*/roles/*.yaml,*/tasks/*.yml,*/tasks/*.yaml,*/handlers/*.yml,*/handlers/*.yaml set filetype=yaml.ansible
+          autocmd BufRead,BufNewFile */group_vars/*.yml,*/group_vars/*.yaml,*/host_vars/*.yml,*/host_vars/*.yaml set filetype=yaml.ansible
+          autocmd BufRead,BufNewFile *playbook.yml,*playbook.yaml,site.yml,site.yaml set filetype=yaml.ansible
+        augroup END
+        augroup filetype_yaml_docker
+          autocmd!
+          autocmd BufRead,BufNewFile *docker-compose*.yml,*docker-compose*.yaml set filetype=yaml.docker
+        augroup END
+        augroup filetype_yaml_helm
+          autocmd!
+          autocmd BufRead,BufNewFile */charts/*.yml,*/charts/*.yaml,*/templates/*.yml,*/templates/*.yaml set filetype=helm
+        augroup END
+        augroup filetype_terraform
+          autocmd!
+          autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform
+        augroup END
+        augroup filetype_force
+          autocmd!
+          autocmd BufRead,BufNewFile *.yaml set filetype=yaml
+          autocmd BufRead,BufNewFile *.tf set filetype=terraform
+          autocmd BufRead,BufNewFile *.helm set filetype=helm
+        augroup END
       '';
       extraLuaConfig = ''
         ${builtins.readFile ./nvim/options.lua}
@@ -82,6 +112,33 @@ in
         require("lualine").setup({
           icons_enabled = true,
         })
+        require('gitsigns').setup()
+        require('mason').setup()
+        require('mason-lspconfig').setup({
+          ensure_installed = { "html", "cssls", "tsserver", "clangd", "helm_ls", "terraformls", "ansiblels", "dockerls", "yamlls", "bashls" },
+          automatic_installation = true,
+        })
+        require("which-key").setup {}
+        local wk = require("which-key")
+        wk.register({
+          f = {
+            name = "file", -- group name
+            s = { ":w<CR>", "Save File" },
+            f = { ":Telescope find_files<CR>", "Find Files" },
+            r = { ":Telescope live_grep<CR>", "Search in Files" },
+          },
+          q = { ":q<CR>", "Quit" },
+          e = { ":NvimTreeToggle<CR>", "Explorer" },
+        }, { prefix = "<leader>" })
+        vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<leader>fm', ':lua vim.lsp.buf.formatting()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<C-c>', 'ggVGy', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<Esc>', ':noh<CR>', { noremap = true, silent = true })
+        augroup SemgrepAutocmd
+          autocmd!
+          autocmd BufWritePost *.py,*.js,*.yaml !semgrep --config=p/security-audit --exclude-dir=node_modules
+        augroup END
       '';
     };
   };
