4a5470820fd0c64b9b034363afd31c9667c7f844
ThauMish <MjA0MjEwMWFjMWY2ZT2YzNjcyZTZkN2M@proton.me>
2024-07-24 11:24:28 +0200

    [ADD] nvim config gennvim + dressingvim

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -279,6 +279,36 @@ local default_plugins = {
   },
 }
 
+{
+    "David-Kunz/gen.nvim",
+    opts = {
+        model = "dolphin-mistral", -- The default model to use.
+        host = "0.0.0.0", -- The host running the Ollama service.
+        port = "11434", -- The port on which the Ollama service is listening.
+        quit_map = "q", -- set keymap for close the response window
+        retry_map = "<c-r>", -- set keymap to re-send the current prompt
+        init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
+        -- Function to initialize Ollama
+        command = function(options)
+            local body = {model = options.model, stream = true}
+            return "curl --silent --no-buffer -X POST http://" .. options.host .. ":" .. options.port .. "/api/chat -d $body"
+        end,
+        display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
+        show_prompt = false, -- Shows the prompt submitted to Ollama.
+        show_model = false, -- Displays which model you are using at the beginning of your chat session.
+        no_auto_close = false, -- Never closes the window automatically.
+        debug = false -- Prints errors and the command which is run.
+    }
+},
+
+{
+  'stevearc/dressing.nvim',
+  opts = {},
+};
+
+
+
+
 local config = require("core.utils").load_config()
 
 if #config.plugins > 0 then
