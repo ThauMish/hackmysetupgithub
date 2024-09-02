865f4f4bbe99444a73635b58d38cd9fddd518a40
ThauMish <MjA0MjEwMWFjMWY2ZT2YzNjcyZTZkN2M@proton.me>
2024-09-02 16:44:50 +0200

    DEL nvim conf

--- config/nvim/lua/custom/treesitter.lua
+++
@@ -1,9 +0,0 @@
-require('custom.treesitter')
-
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "terraform", "lua", "python" }, -- Exemple de langages
-  highlight = {
-    enable = true,
-  },
-  -- Ajoutez ici d'autres configurations nvim-treesitter si nécessaire
-}
