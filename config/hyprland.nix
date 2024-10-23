5e590838caea95363a2352f375a4490d8dafd66d
ThauMish <MjA0MjEwMWFjMWY2ZT2YzNjcyZTZkN2M@proton.me>
2024-10-23 11:15:52 +0200

    [UPDATE] fix sound

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -48,7 +48,6 @@ with lib;
           exec-once = killall -q swaync;sleep .5 && swaync
           exec-once = nm-applet --indicator
           exec-once = lxqt-policykit-agent
-          exec-once = sleep 1.5 && swww img /home/${username}/Pictures/Wallpapers/beautifulmountainscape.jpg
           monitor=,preferred,auto,1
           ${extraMonitorSettings}
           general {
